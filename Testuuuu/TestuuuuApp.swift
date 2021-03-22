1916980659498f0b1de90ce1246a4921c4ce8267
itnams <68367990+itnams@users.noreply.github.com>
2021-03-22 09:44:04 +0700

    Initial Commit

---
+++ Testuuuu/TestuuuuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestuuuuApp.swift
+//  Testuuuu
+//
+//  Created by namnguyen on 22/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestuuuuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
